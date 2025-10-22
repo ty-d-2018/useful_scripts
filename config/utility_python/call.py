784569e8a9054250c13b70c22769f4defc42e045
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-10-22 02:17:01 -0600

    added commands and excuting shell scripts

--- config/utility_python/call.py
+++ config/utility_python/call.py
@@ -4,4 +4,22 @@ class CallingCommand:
     def __init__(self, commandMap):
         self.commands = []
         for key, value in commandMap.items():
-            pass
+            tempC = []
+            tempC.append(key)
+            tempC.append(value)
+            self.commands.append(tempC)
+        
+    def execute_command(self, c):
+        return subprocess.run(c, capture_output=True, text=True, check=True)
+
+    def run_all_commands(self):
+        results = []
+        for command in self.commands:
+            results.append(self.execute_command(command))
+        
+        return results
+
+    def run_a_command(self, i):
+        result = self.execute_command(self.commands[i])
+
+        return result
