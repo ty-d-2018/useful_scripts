9f7bac70900f4f122cbf3cc0f73ea05897977f61
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-10-23 03:44:15 -0600

    standarized into blocks for arguments across the bash prompt

---
+++ config/utility_python/script_block.py
@@ -0,0 +1,34 @@
+from argument import Argument
+
+class Blocks(Argument):
+    def __init__(self, tags):
+        super().__init__(tags)
+        self.k = var(self.param_keys).keys()
+        self.i = 0
+
+    def get_block(self):
+        if self.is_empty():
+            return None
+        block = self.get_option(self.k[self.i])
+        self.increase_count()
+
+        return block
+
+    def increase_count(self):
+        if self.i >= len(self.k):
+            self.i = 0
+        else:
+            self.i = self.i + 1
+
+    def loop_blocks(self, action):
+        block = self.get_block()
+        action(block)
+
+        while self.i > 0:
+            block = self.get_block()
+            action(block)
+
+    def is_empty(self):
+        if len(self.k) == 0:
+            return True
+        return False
