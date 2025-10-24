f35688459585b2cc9bbbb29fc456b5ea6d32005b
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-10-24 01:57:38 -0600

    reworked BlocksArgument

--- config/utility_python/block.py
+++ config/utility_python/block.py
@@ -1,7 +1,7 @@
 from argument import Argument
 
 class Blocks:
-    def __init__(self, pairs):
+    def __init__(self, pairs={}):
         self.pairs = {}
         self.k = None
         self.i = 0
@@ -44,39 +44,7 @@ class Blocks:
             return True
         return False
 
-class BlockArgument(Argument):
+class BlocksArgument(Blocks):
     def __init__(self, tags):
-        super().__init__(tags)
-        self.pArgs = self.get_parsed()
-        self.k = var(self.pArgs).keys()
-        self.i = 0
-
-    def get_option(self, name):
-        optionValue = getattr(self.argParse, name)
-        return optionValue
-
-    def get_block(self):
-        if self.is_empty():
-            return None
-        name = self.k[self.i]
-        block = get_option(name)
-        self.increase_count()
-
-        return (name, block)
-
-    def increase_count(self):
-        if self.i >= len(self.k):
-            self.i = 0
-        else:
-            self.i = self.i + 1
-
-    def loop_blocks(self, activity):
-        name, block = self.get_block()
-        while self.i > 0:
-            activity(name, block)
-            name, block = self.get_block()
-
-    def is_empty(self):
-        if len(self.k) == 0:
-            return True
-        return False
+        self.arguments = Argument(tags)
+        Blocks.__init__(self.arguments.get_parsed())
