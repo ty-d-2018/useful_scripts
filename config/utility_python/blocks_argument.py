17c8d9de078804a72f6591ede64e830c1f04d6ea
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-10-24 02:01:30 -0600

    fixed bug

--- config/utility_python/blocks_argument.py
+++ config/utility_python/blocks_argument.py
@@ -3,5 +3,6 @@ from block.py import Blocks
 
 class BlocksArgument(Blocks):
     def __init__(self, tags, argument):
-        self.arguments = argument.set_tags(tags)
+        self.arguments = argument
+        self.arguments.set_tags(tags)
         Blocks.__init__(self.arguments.get_parsed())
