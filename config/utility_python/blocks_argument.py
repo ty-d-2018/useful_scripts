a3718798449534156357cf3a6d5268bbf6119c58
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-10-24 02:00:47 -0600

    made agrument open to sub class implmentation

--- config/utility_python/blocks_argument.py
+++ config/utility_python/blocks_argument.py
@@ -2,6 +2,6 @@ from argument.py import Argument
 from block.py import Blocks
 
 class BlocksArgument(Blocks):
-    def __init__(self, tags):
-        self.arguments = Argument(tags)
+    def __init__(self, tags, argument):
+        self.arguments = argument.set_tags(tags)
         Blocks.__init__(self.arguments.get_parsed())
