1182a1522d6a1e4eeecc4be8f78dfa8d571ef836
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-10-24 01:34:26 -0600

    deleted old CallingCommand

--- config/utility_python/call.py
+++ config/utility_python/call.py
@@ -43,30 +43,3 @@ class Volunteer:
             handles.append(process)
 
         return handles
-
-class CallingCommand:
-    def __init__(self, commandMap):
-        self.commands = []
-        for key, value in commandMap.items():
-            tempC = []
-            tempC.append(key)
-            tempC.append(value)
-            self.commands.append(tempC)
-        
-    def execute_command(self, c):
-        return subprocess.run(c, capture_output=True, text=True, check=True)
-
-    def run_all_commands(self):
-        results = []
-        for command in self.commands:
-            results.append(self.execute_command(command))
-        
-        return results
-
-    def run_a_command(self, i):
-        result = self.execute_command(self.commands[i])
-
-        return result
-
-    def get_commands(self):
-        return self.commands
