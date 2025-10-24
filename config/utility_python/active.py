6f57696b453d6ca536ec06bfc578da292bae05c0
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-10-24 01:36:01 -0600

    removed old methods for activity

--- config/utility_python/active.py
+++ config/utility_python/active.py
@@ -2,25 +2,9 @@
 class Activity:
     def __init__(self, name):
         self.name = name
-        self.tags = {}
-
+        
     def get_label(self):
         return self.name
 
-    def add_tag(self, key, value):
-        self.tags[key] = value
-
-    def set_tag(self, tags):
-        self.tags = tags
-
-    def reset_tags(self):
-        self.tags = {}
-
     def read_block(self, name, block):
         return f"I have the block named: {name}"
-
-    def get_tags(self):
-        return self.tags
-
-    def send_tags(self, resolution):
-        resolution.set_tags(self.get_tags())
