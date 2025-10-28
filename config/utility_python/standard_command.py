3282073f3e196e4f1bc48e4334f78ecd1c86d981
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-10-27 20:33:28 -0600

    worked on template activity

--- config/utility_python/standard_command.py
+++ config/utility_python/standard_command.py
@@ -1,6 +1,39 @@
 from block.py import Blocks
 from active.py import Activity
+from template.py import Template
 
+def create_map():
+    arg_map = {
+        "#FILENAME": "file-path",
+        "#NUMBER": "numerial",
+        "#DIRECTORY": "directory-path"
+    }
+
+    return arg_map
+
+class TemplateActivity(Activity):
+    def __init__(self):
+        super().__init__("template-activity")
+        self.template = Template()
+        self.keys = {}
+
+    def set_table(self, subjects, values):
+        for i in range(0, len(subjects)):
+            template.add_row(subjects[i], values[i])
+
+    def set_keys(self):
+        arg_map = create_map()
+        column = self.template.get_column("subject")
+        for i in range(0, len(column)):
+            if column[i] in arg_map:
+                self.keys[column[i]] = i
+
+    def set_keys_and_table(self, subjects, values):
+        self.set_table(subjects, values)
+        self.set_keys()
+
+    def read_block(self, name, block):
+        blocks = self.template.get_value(self.keys[name])
 
 class CommandBlock(Blocks):
     def __init__(self):
@@ -16,13 +49,9 @@ class CommandBlock(Blocks):
         self.set_pair_keys(command_pairs)
 
     def create_value(self, value_str):
-        possible_values = {
-            "#FILENAME": Value("file-path"),
-            "#NUMBER": Value("numerial"),
-            "#DIRECTORY": Value("directory-path")
-        }
+        possible_values = create_map()
 
         if value_string in possible_values:
-            return possible_values[value_string]
+            return Value(possible_values[value_string])
         else:
             return None
