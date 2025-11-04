6ac5244a5a7576fb4450ad801f6b75670c9c1693
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-11-03 23:02:42 -0700

    setting up the script to launch render frame with the final launch command call

--- config/render_scene.py
+++ config/render_scene.py
@@ -1,1 +1,15 @@
-from utility_python
+from utility_python.render_python import RenderFrame
+
+def create_render_frame(json_src_file, blend_src_file, render_output_file):
+    render_frame = RenderFrame(json_src_file, blender_src_file, render_output_file)
+    
+    render_frame.setup_command_block()
+
+def run_render(render_frame):
+    render_frame.activity_block()
+    render_frame.run_render()
+
+
+def launch():
+    render_frame = create_render_frame()
+    run_render(render_frame)
