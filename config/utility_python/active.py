6cf225a81fbae933575ce647222ab27054becad4
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-11-14 16:50:09 -0700

    added read_set_block and read_sequence to the base Activity class

--- config/utility_python/active.py
+++ config/utility_python/active.py
@@ -8,3 +8,9 @@ class Activity:
 
     def read_block(self, name, block):
         return f"I have the block named: {name}"
+
+    def read_set_block(self, allBlocks):
+        pass
+
+    def read_sequence(self, sequence):
+        pass
